70589875a31299a0fafdfd3112760ff83c18679a
rpg2014 <rpg2014@users.noreply.github.com>
2023-09-17 10:18:21 -0700

    Added name to copy button

--- app/components/CodeBlock.tsx
+++ app/components/CodeBlock.tsx
@@ -26,7 +26,7 @@ export const CodeBlock = ({ text, inline, children }: CodeBlockProps) => {
     setTimeout(() => setCopied(false), 1000);
   };
   const CopyButton = ({ small }: { small?: boolean } = {}) => (
-    <button disabled={copied} data-text={text} onClick={copyText} className={`${styles.copyButton}  ${small && styles.small} ${copied ? styles.disabled : ""}`}>
+    <button name="copy" disabled={copied} data-text={text} onClick={copyText} className={`${styles.copyButton}  ${small && styles.small} ${copied ? styles.disabled : ""}`}>
       {copied ? (
         "✓"
       ) : (
